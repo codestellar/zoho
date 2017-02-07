fe1a4977df32f5946f8e0618262a336e03a4c7d1
Gaurav Madaan <gaurav.madaan@technossus.com>
2017-02-07 12:43:19 +0530

    Minor change

--- leads.js
+++ leads.js
@@ -1,6 +1,6 @@
 var Zoho = require('zoho');
 var crm = new Zoho.CRM({
-  authtoken: 'c9a5be8192b22672d6e921a9b1e9644f'
+  authtoken: ''
 });
 crm.getRecords('leads', function (err, data) {
   if (err) {
